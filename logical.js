6c6f5a664e449c3f451b5fc9b1d6a8a997230e7f
Gurulakshmi-123 <gurulakshmi2897@gmail.com>
2022-08-17 16:09:58 +0530

    logical operators concept

---
+++ logical.js
@@ -0,0 +1,18 @@
+if(10>20 && 10<20 && 20>30)
+{
+    console.log(true)
+}
+else{
+    console.log(false)              //all conditions should satisfy
+}
+
+
+
+
+if(10>20 || 10<20 || 20>30)
+{
+    console.log(true)
+}
+else{
+    console.log(false)
+}                                      //any condition can satisfy
